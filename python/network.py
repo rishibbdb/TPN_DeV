ecebda09b44443c8c63b2114ab1a1d1d29bb58a6
Hans Niederhausen <niederh7@msu.edu>
2024-05-09 15:56:59 -0700

    make network code more useable

---
+++ python/network.py
@@ -0,0 +1,70 @@
+import numpy as np
+import jaxlib
+import jax.numpy as jnp
+from typing import List, Tuple, Any
+import os
+import jax
+from functools import partial
+
+
+class TriplePandleNet:
+    def __init__(self, bpath: str) -> None:
+        self.params = self._get_network_params(bpath)
+
+    def eval(self, x: jaxlib.xla_extension.ArrayImpl) \
+            -> jaxlib.xla_extension.ArrayImpl:
+        """
+        """
+        return _eval_network(x, self.params)
+
+    def eval_on_batch(self, x: jaxlib.xla_extension.ArrayImpl) \
+            -> jaxlib.xla_extension.ArrayImpl:
+        """
+        """
+        return _eval_network_on_batch(x, self.params)
+
+    def get_network_params(self) -> Tuple[Tuple[jaxlib.xla_extension.ArrayImpl]]:
+        return self.params
+
+    def _get_network_params(self, bpath: str) -> Tuple[Tuple[jaxlib.xla_extension.ArrayImpl]]:
+        """
+        """
+        params = []
+
+        for i in range(0, 9):
+            layer_weights = np.load(os.path.join(bpath, f'dense_{i}_weights.npy'))
+            layer_bias = np.load(os.path.join(bpath, f'dense_{i}_bias.npy'))
+            params.append((jnp.array(layer_weights), jnp.array(layer_bias)))
+
+        for param in params:
+            param[0].devices()
+            param[1].devices()
+
+        return tuple(params)
+
+
+@jax.jit
+def _eval_network(x: jaxlib.xla_extension.ArrayImpl,
+        params: Tuple[Tuple[jaxlib.xla_extension.ArrayImpl]]) \
+                -> jaxlib.xla_extension.ArrayImpl:
+    """
+    """
+
+    x = jnp.tanh(jnp.dot(x, params[0][0]) + params[0][1])
+
+    # residual block 1
+    y = jnp.tanh(jnp.dot(x, params[1][0]) + params[1][1])
+    y = jnp.tanh(jnp.dot(y, params[2][0]) + params[2][1])
+    x = jnp.tanh(jnp.dot(y, params[3][0]) + params[3][1]) + x
+
+    # residual block 2
+    y = jnp.tanh(jnp.dot(x, params[4][0]) + params[4][1])
+    y = jnp.tanh(jnp.dot(y, params[5][0]) + params[5][1])
+    x = jnp.tanh(jnp.dot(y, params[6][0]) + params[6][1]) + x
+
+    # outputs
+    y = jnp.tanh(jnp.dot(x, params[7][0]) + params[7][1])
+    z = jnp.dot(y, params[8][0]) + params[8][1]
+    return z
+
+_eval_network_on_batch = jax.jit(jax.vmap(_eval_network, (0, None), 0))
