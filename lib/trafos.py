2571187b5665196c1bc93802f331435973745f34
Hans Niederhausen <niederh7@msu.edu>
2024-05-20 17:04:53 -0700

    working time sampled

--- lib/trafos.py
+++ lib/trafos.py
@@ -4,6 +4,7 @@ import jax.numpy as jnp
 @jax.jit
 def transform_network_inputs(x):
 	# 0: dist, 1: rho, 2: z, 3: zenith, 4: azimuth
+    # in units of m and radians
     km_scale = 1000
     dist = x[0]
     rho = x[1]
@@ -16,9 +17,9 @@ def transform_network_inputs(x):
     x2 = jnp.sin(rho)
     x3 = z / km_scale
 
-    z = jnp.cos(jnp.deg2rad(zenith))
-    x = jnp.sin(jnp.deg2rad(zenith)) * jnp.cos(jnp.deg2rad(azimuth))
-    y = jnp.sin(jnp.deg2rad(zenith)) * jnp.sin(jnp.deg2rad(azimuth))
+    z = jnp.cos(zenith)
+    x = jnp.sin(zenith) * jnp.cos(azimuth)
+    y = jnp.sin(zenith) * jnp.sin(azimuth)
     return jnp.array([x0, x1, x2, x3, z, x, y])
 
 transform_network_inputs_v = jax.jit(jax.vmap(transform_network_inputs, 0, 0))
@@ -31,6 +32,9 @@ def transform_network_outputs(x):
     return logits, a, b
 
 transform_network_outputs_v = jax.jit(jax.vmap(transform_network_outputs, 0, 0))
+
+"""
+use transform_network_inputs and _outputs instead.
 
 @jax.jit
 def transform_dimensions(dist, rho, z, zenith, azimuth, km_scale = 1000):
@@ -64,3 +68,4 @@ def transform_dimensions_vec(x, km_scale=1000):
     x5 = jnp.sin(jnp.deg2rad(zenith)) * jnp.cos(jnp.deg2rad(azimuth))
     x6 = jnp.sin(jnp.deg2rad(zenith)) * jnp.sin(jnp.deg2rad(azimuth))
     return jnp.concatenate([x0, x1, x2, x3, x4, x5, x6], axis=1)
+"""
---
+++ time_sampler.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import jax
+import jax.numpy as jnp
+from tensorflow_probability.substrates import jax as tfp
+tfd = tfp.distributions
+
+from lib.geo import cherenkov_cylinder_coordinates_v
+from lib.geo import rho_dom_relative_to_track_v
+from lib.geo import get_xyz_from_zenith_azimuth
+from lib.trafos import transform_network_outputs_v, transform_network_inputs_v
+from typing import Any
+
+
+def sample_times(event_data: pd.DataFrame,
+				track_pos: jnp.array,
+				track_dir: jnp.array,
+				track_time: float,
+				network: Any,
+                prng_key: jax.random.key) -> jnp.array:
+    """
+	track_dir: zenith, azimuth of src direction in radians
+    """
+
+    dom_positions = jnp.array(event_data[['x', 'y', 'z']].to_numpy())
+    charges = np.round(event_data['charge'].to_numpy()+0.5).astype(int)
+    track_dir_xyz = get_xyz_from_zenith_azimuth(track_dir)
+
+    geo_time, closest_approach_dist, closest_approach_z = \
+    cherenkov_cylinder_coordinates_v(dom_positions,
+                                     track_pos,
+                                     track_dir_xyz)
+
+    closest_approach_rho = rho_dom_relative_to_track_v(dom_positions, track_pos, track_dir_xyz)
+
+    track_zenith = track_dir[0]
+    track_azimuth = track_dir[1]
+    x = jnp.column_stack([closest_approach_dist,
+                      closest_approach_rho,
+                      closest_approach_z,
+                      jnp.repeat(track_zenith, len(closest_approach_dist)),
+                      jnp.repeat(track_azimuth, len(closest_approach_dist))])
+
+    x_prime = transform_network_inputs_v(x)
+    y_pred = network.eval_on_batch(x_prime)
+    logits, av, bv = transform_network_outputs_v(y_pred)
+
+    gm = tfd.MixtureSameFamily(
+              mixture_distribution=tfd.Categorical(
+                  logits=logits
+                  ),
+              components_distribution=tfd.Gamma(
+                concentration=av,
+                rate=bv,
+                force_probs_to_zero_outside_support=True
+                  )
+            )
+
+    n_tot = np.amax(charges)
+    times = np.array(gm.sample(sample_shape = n_tot, seed = prng_key))
+    print(times.shape)
+
+    n_doms = times.shape[1]
+    first_times = np.zeros(n_doms)
+
+    for i in range(n_doms):
+        first_times[i] = np.amin(times[:charges[i], i])
+
+    return first_times + geo_time + track_time
