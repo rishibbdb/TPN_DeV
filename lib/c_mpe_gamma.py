071f78cc52521d8d1cb980ecf300028b719d71ce
Hans Niederhausen <niederh7@msu.edu>
2024-08-01 19:08:02 -0700

    remove dead code

--- lib/c_mpe_gamma.py
+++ lib/c_mpe_gamma.py
@@ -68,42 +68,6 @@ def c_multi_gamma_mpe_prob_pure_jax(x, mix_probs, a, b, n, sigma=3.0):
     sfs_power_n = jnp.power(sfs, n)
     return jnp.sum(n_pdf * (sfs_power_n[:-1]-sfs_power_n[1:]))
 
-#def c_multi_gamma_mpe_prob_pure_jax(x, mix_probs, a, b, n, sigma=3.0):
-# ### attempt at keeping the peak at 0 within region of integration.
-#
-#    nmax = 3.5
-#    nint = 7
-#    eps = 1.e-6
-#    split_point = 1.0
-#
-#    # integrate around gaussian region
-#    xmin = jnp.max(jnp.array([split_point, x - nmax * sigma]))
-#    xmax = jnp.max(jnp.array([jnp.array(nmax * sigma), x + nmax * sigma]))
-#
-#    xvals = jnp.linspace(xmin, xmax, nint)
-#    n_pdf = norm_pdf(0.5*(xvals[:-1]+xvals[1:]), loc=x, scale=sigma)
-#
-#    a_e = jnp.expand_dims(a, axis=-1)
-#    b_e = jnp.expand_dims(b, axis=-1)
-#    mix_probs_e = jnp.expand_dims(mix_probs, axis=-1)
-#
-#    xvals_e = jnp.expand_dims(xvals, axis=0)
-#    sfs = jnp.sum(mix_probs_e * gamma_sf(xvals_e, a_e, scale=1./b_e), axis=0)
-#    sfs_power_n = jnp.power(sfs, n)
-#    gaussian_region = jnp.sum(n_pdf * (sfs_power_n[:-1]-sfs_power_n[1:]))
-#    gaussian_region = jnp.clip(gaussian_region, min=0.0)
-#
-#    # integrate before gaussian region
-#    xvals = jnp.linspace(eps, split_point, nint)
-#    xvals_e = jnp.expand_dims(xvals, axis=0)
-#    n_pdf = norm_pdf(0.5*(xvals[:-1]+xvals[1:]), loc=x, scale=sigma)
-#    sfs = jnp.sum(mix_probs_e * gamma_sf(xvals_e, a_e, scale=1./b_e), axis=0)
-#    sfs_power_n = jnp.power(sfs, n)
-#    before_region = jnp.sum(n_pdf * (sfs_power_n[:-1]-sfs_power_n[1:]))
-#    before_region = jnp.clip(before_region, min=0.0)
-#
-#    return gaussian_region+before_region
-
 c_multi_gamma_mpe_prob_pure_jax_v = jax.vmap(c_multi_gamma_mpe_prob_pure_jax, (0, 0, 0, 0, 0, None), 0)
 
 
